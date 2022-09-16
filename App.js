66daaff99508b5ce3dd77cb0c49be1c6c83953e0
Matthieu Dejean <matthieudejean2@gmail.com>
2022-09-16 11:58:52 +0200

    Ceate tab menu for display two screens and create resarch input to call city api

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as Location from "expo-location";
+import { Ionicons } from "@expo/vector-icons";
 import Moment from "moment";
 import "moment/locale/fr";
 
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const apiKey = "31211e007ef432a5605ec6173339d1b5";
 const urlLondon = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=31211e007ef432a5605ec6173339d1b5`;
@@ -22,14 +26,50 @@ axios
   })
   .then(function () {});
 
-export default function App() {
+function HomeScreen() {
+  const [cityUser, onChangeCityUser] = useState("");
+  const [mainTemp, setMainTemp] = useState(0);
+  const [city, setCity] = useState("");
+
+  const getValues = () => {
+    const cityCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityUser}&appid=${apiKey}&units=metric`;
+    axios
+      .get(cityCall)
+      .then(function (response) {
+        console.log("city call data", response.data);
+        setCity(response.data.name);
+        setMainTemp(response.data.main.temp);
+      })
+      .catch(function (error) {
+        console.log("catch", error);
+      })
+      .then(function () {});
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Météo</Text>
+      <Text>Voici ma première app React Native</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(cityUser) => onChangeCityUser(cityUser)}
+      />
+      <Button title="Valider" onPress={() => getValues()} />
+      <Text>La météo de {city}</Text>
+      <Text>
+        Il fait {mainTemp}° à {city}
+      </Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
   const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
   const [mainTemp, setMainTemp] = useState(0);
   const [city, setCity] = useState("");
   const [weatherMain, setWeatherMain] = useState("");
   const [weatherIcon, setWeatherIcon] = useState("");
-  const formatDate = Moment().format("LLL");
 
   useEffect(() => {
     (async () => {
@@ -61,7 +101,6 @@ export default function App() {
         .then(function () {});
     })();
   }, []);
-
   return (
     <View style={styles.container}>
       <Text>Météo de Bordeaux {city}</Text>
@@ -80,6 +119,44 @@ export default function App() {
   );
 }
 
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Details") {
+              iconName = focused ? "cloud-sharp" : "cloud-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "About" }}
+        />
+        <Tab.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{ title: "Détails" }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -90,5 +167,12 @@ const styles = StyleSheet.create({
   tinyLogo: {
     width: 250,
     height: 250,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
